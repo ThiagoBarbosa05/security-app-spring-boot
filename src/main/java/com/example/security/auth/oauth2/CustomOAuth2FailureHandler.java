384b148960fda4aebc4e3620cd3fc6582573bd14
thiago barbosa <thiagon16@hotmail.com>
2025-02-18 00:09:31 -0300

    fix: redirect url in custom oauth failure handler

--- src/main/java/com/example/security/auth/oauth2/CustomOAuth2FailureHandler.java
+++ src/main/java/com/example/security/auth/oauth2/CustomOAuth2FailureHandler.java
@@ -3,6 +3,7 @@ package com.example.security.auth.oauth2;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -12,12 +13,15 @@ import java.io.IOException;
 @Component
 public class CustomOAuth2FailureHandler implements AuthenticationFailureHandler {
 
+  @Value("${base.url}")
+  private String baseUrl;
+
   @Override
   public void onAuthenticationFailure(
     HttpServletRequest request,
     HttpServletResponse response,
     AuthenticationException exception
   ) throws IOException, ServletException {
-    response.sendRedirect("http://localhost:3000?origin_error=login");
+    response.sendRedirect(baseUrl + "?error=oauth_failure");
   }
 }
